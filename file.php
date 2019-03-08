51723958ab61a5988a322ab7cd6054932730a992
Cameron Jones <c.jones@digitalmakers.io>
2019-03-08 14:47:57 +1000

    Initial commit

---
+++ file.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * My testing file
+ *
+ * @package ci-testing
+ */
+
+/**
+ * Some testing function
+ *
+ * @return bool
+ */
+function ci_testing() {
+	// Some inline comment
+	return true;
+}
